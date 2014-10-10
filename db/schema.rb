ba33b56ac1568c3c842813e91a34c3770545d0c8
Boris Adimov <boris@adimov.ru>
2014-10-10 10:28:45 +0400

    Сделал страницы блога динамическими

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009120639) do
+ActiveRecord::Schema.define(version: 20141009174505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
