e1536435ac50b33e0504adeff688505e69432581
Boris Adimov <boris@adimov.ru>
2014-12-08 12:10:06 +0300

    DEAL WITH IT

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,12 +2,18 @@ class ShopController < ApplicationController
 
   def index
     # @gallery = ShopGallery.first.try :gallery
-    @category = ShopCategory.find_by(alias: params[:category])
-    # @items = ShopItem.all.order(sort_index: :desc)
-    if @category.name = "Sale"
+    category = params[:category]
+    if category == "sale"
       @items = ShopItem.where(sale: :true).try(:order, sort_index: :desc)
+
     else
+      @category = ShopCategory.find_by(alias: category)
       @items = @category.try(:shop_items).try(:order, sort_index: :desc) || ShopItem.all.try(:order, sort_index: :desc)
+    end
+    # @items = ShopItem.all.order(sort_index: :desc)
+    # if @category.name = "Sale"
+    #
+    # else
 
 
   end
